f2a0ca41a83d3b9e75fc915dad79eee66f08dab4
Furkan <gami.furkan@gmail.com>
2022-11-29 15:41:39 +0300

    Project Completed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import app from "./server.js";
 import mongodb from "mongodb";
-
-// import ReviewsDAO from "./dao/reviewsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 const MongoClient = mongodb.MongoClient;
 const mongo_username = process.env.MONGO_USERNAME;
@@ -21,6 +20,7 @@ MongoClient.connect(uri, {
     process.exit(1);
   })
   .then(async (client) => {
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     });
